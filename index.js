cb12ec24291b3fbdc5ea009aeb8f152d12e75bb9
Harald Kirschner <hkirschner@mozilla.com>
2017-06-12 13:24:00 -0700

    force https

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ const report = require('./api/report');
 
 app.prepare().then(() => {
   const server = express();
+  server.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+      // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
+      res.header('Strict-Transport-Security', 'max-age=15768000');
+      // https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(301, `https://${req.get('Host')}${req.url}`);
+      }
+    }
+    return next();
+  });
   server.use(bodyParser.json());
   server.set('json replacer', replacer);
   server.set('redis', client);
